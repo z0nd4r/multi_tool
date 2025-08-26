1afa94fd304ca6d80545a780b876657671054c89
zondar__ <kirito98730tkm@gmail.com>
2025-08-26 22:48:39 +0200

    Minor changes

--- window/__init__.py
+++ window/__init__.py
@@ -1,5 +1,5 @@
 from .number_systems import NumberSystems
 from .regular_calculator import RegularCalculator
 from .image_converter import ImageConverter
-from .menu import Menu
+# from .menu import Menu
 from .text_file_converter import TextFileConverter
