8b79eeef1d487de7fdd010a185cabd165b7b53c3
zondar__ <kirito98730tkm@gmail.com>
2025-09-11 20:38:02 +0200

    Minor changes

--- window/little_things/random_num_generator.py
+++ window/little_things/random_num_generator.py
@@ -1,4 +1,5 @@
 import re
+import tkinter as tk
 from tkinter import ttk, messagebox
 from config import get_random_num
 
@@ -11,12 +12,12 @@ class RandomNumberGenerator:
     def _create_interface(self):
         check_1 = (self.root.register(self.is_valid_1), "%P")
 
-        self.root.columnconfigure(0, weight=1)  # ✅ Левый распор
+        self.root.columnconfigure(0, weight=1)  # левый отступ
         self.root.columnconfigure(1, weight=0)  #
-        self.root.columnconfigure(2, weight=0)  # Центральные столбцы
+        self.root.columnconfigure(2, weight=0)  # центральные столбцы
         self.root.columnconfigure(3, weight=0)  #
         self.root.columnconfigure(4, weight=0)  #
-        self.root.columnconfigure(5, weight=1)
+        self.root.columnconfigure(5, weight=1)  # правый отступ
 
         self.label_ran_num = ttk.Label(self.root, text='Случайное число:')
         self.label_ran_num.grid(row=0, column=1, pady=10, columnspan=4)
@@ -31,27 +32,58 @@ class RandomNumberGenerator:
                                  command=self._show_generation_result)
         self.button.grid(row=2, column=1, columnspan=4)
 
-        self.label_range = ttk.Label(self.root, text='Диапазон:')
-        self.label_range.grid(row=3, column=1, columnspan=4, pady=10)
+        self.type_of_range = tk.StringVar(value='range')
 
-        self.label_range_of = ttk.Label(self.root, text='от')
-        self.label_range_of.grid(row=4, column=1, padx=(0, 5))
-        self.entry_range_of = ttk.Entry(self.root,
+        self.radio_button_range = ttk.Radiobutton(self.root,
+                                                 text='из диапазона',
+                                                 value='range',
+                                                 variable=self.type_of_range,
+                                                 command=self._select_type_of_range)
+        self.radio_button_range.grid(row=3, column=2, pady=10, sticky='we')
+
+        self.radio_button_range = ttk.Radiobutton(self.root,
+                                                 text='из списка',
+                                                 value='list',
+                                                 variable=self.type_of_range,
+                                                 command=self._select_type_of_range)
+        self.radio_button_range.grid(row=3, column=3, pady=10, sticky='we')
+
+        # self.label_range = ttk.Label(self.root, text='Диапазон:')
+        # self.label_range.grid(row=3, column=1, columnspan=4, pady=10)
+
+        # фрейм для диапазона
+        self.range_frame = ttk.Frame(self.root)
+        self.range_frame.grid(row=4, column=1, columnspan=4, pady=10)
+
+        self.label_range_of = ttk.Label(self.range_frame, text='от')
+        self.label_range_of.grid(row=0, column=1, padx=(0, 5))
+        self.entry_range_of = ttk.Entry(self.range_frame,
                                         width=5,
                                         validate='key',
                                         validatecommand=check_1)
-        self.entry_range_of.grid(row=4, column=2, padx=(0, 5))
+        self.entry_range_of.grid(row=0, column=2, padx=(0, 5))
         self.entry_range_of.insert(0, 10)
 
-        self.label_range_to = ttk.Label(self.root, text='до')
-        self.label_range_to.grid(row=4, column=3, padx=(0, 5))
-        self.entry_range_to = ttk.Entry(self.root,
+        self.label_range_to = ttk.Label(self.range_frame, text='до')
+        self.label_range_to.grid(row=0, column=3, padx=(0, 5))
+        self.entry_range_to = ttk.Entry(self.range_frame,
                                         width=5,
                                         validate='key',
                                         validatecommand=check_1)
-        self.entry_range_to.grid(row=4, column=4)
+        self.entry_range_to.grid(row=0, column=4)
         self.entry_range_to.insert(0, 100)
 
+        # фрейм для диапазона из списка
+        self.range_list_frame = ttk.Frame(self.root)
+        self.range_list_frame.grid(row=4, column=1, columnspan=4, pady=10)
+
+        self.entry_list = ttk.Entry(self.range_list_frame)
+        self.entry_list.grid(row=0, column=1)
+        self.label_list = ttk.Label(self.range_list_frame, text='напишите числа через запятую')
+        self.label_list.grid(row=2, column=1)
+
+
+        self.range_list_frame.grid_remove()
 
     def _show_generation_result(self):
         try:
@@ -67,3 +99,12 @@ class RandomNumberGenerator:
 
     def is_valid_1(self, newval):
         return re.match(r'^\d*$', newval) is not None
+
+    def _select_type_of_range(self):
+        print(self.type_of_range.get())
+        if self.type_of_range.get() == 'range':
+            self.range_frame.grid()
+            self.range_list_frame.grid_remove()
+        elif self.type_of_range.get() == 'list':
+            self.range_list_frame.grid()
+            self.range_frame.grid_remove()
